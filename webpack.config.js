face72da3852fade654b5163445dd5e970fb0a23
bmacheski <bwm0007@gmail.com>
2015-08-27 00:00:50 -0500

    add dev server and hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-  entry: "./app/App.js",
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/App'
+  ],
   output:{
-    filename: "public/bundle.js"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel'
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
       },
       {
         test: /\.css$/,
